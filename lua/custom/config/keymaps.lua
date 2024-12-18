9a09df2e5971a4cc4ec9a07534f72bee5a380bb8
asabla <1063362+asabla@users.noreply.github.com>
2024-12-18 19:49:55 +0100

    Changed theme and fixed a few keymaps

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -1,3 +1,9 @@
+-- Disable Q as a register keymap
+vim.keymap.set('n', 'Q', '<Nop>')
+
+-- Binds sessionizer keymaps
+vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>', { desc = 'Open tmux sessionizer' })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
@@ -7,24 +13,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
---
+
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---  See `:help wincmd` for a list of all window commands
--- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
--- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
--- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
--- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Removes current bindnings for hjkl
 vim.keymap.set({ 'n', 'v' }, 'h', '<Nop>')
@@ -63,29 +55,13 @@ vim.keymap.set('n', 'N', 'Nzzzv', opts)
 -- Copilot keymaps
 vim.keymap.set({ 'n', 'v', 'i' }, '<M-m>', '<Nop>')
 vim.keymap.set({ 'n', 'v', 'i' }, '<M-n>', '<Nop>')
--- vim.keymap.set('n', '<M-m>', require('copilot.suggestion').accept, { desc = 'Accept current suggestion' })
--- vim.keymap.set('n', '<M-m>', require('custom.plugins.copilot').accept, { desc = 'Accept current suggestion' })
--- vim.keymap.set('n', '<M-n>', require('copilot.suggestion').next, { desc = 'Show next suggestion' })
 
 -- Fix ^M in current buffer
 vim.api.nvim_set_keymap('n', '<leader>dtu', ':%s/\\r//g<CR>', { noremap = true, silent = false, desc = 'Remove ^M from current buffer' })
 
 -- Trouble keys
--- TODO: something ain't right with the commented out bindings
-
--- vim.keymap.set('n', '<leader>tt', function()
---   require('trouble').toggle 'diagnostics'
--- end, { desc = '[T]oggle [T]rouble' })
-
 vim.keymap.set({ 'n', 'v' }, '<leader>tt', '<cmd>Trouble diagnostics toggle<cr>', { desc = '[T]oggle [T]rouble' })
 vim.keymap.set({ 'n', 'v' }, '<leader>tq', '<cmd>Trouble quickfix toggle<cr>', { desc = '[T]oggle trouble [Q]uickfix' })
-
--- vim.keymap.set('n', '<leader>tq', function()
---   require('trouble').toggle 'quickfix'
--- end, { desc = '[T]oggle Trouble [Q]uickfix' })
-
--- vim.keymap.set("n", "<leader>tw", function() require("trouble").toggle("workspace_diagnostics") end, { desc = '[T]oggle [W]orkspace diagnostic' })
--- vim.keymap.set("n", "<leader>td", function() require("trouble").toggle("diagnostics") end, { desc = '[T]oggle [D]ocument diagnostic' })
 
 -- Oil keys
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
